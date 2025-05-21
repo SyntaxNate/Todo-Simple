1206f58ee5c28bc10cd3b2885894e06dfe1dbb7d
SyntaxNate <new-shutter.nbt@gmail.com>
2025-05-20 20:41:47 -0400

    button

---
+++ UI-Interactive/interactive-gui/src/App.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div style={{ margin: 40 }}>
+      <h1>Hello, Material UI!</h1>
+      <Button variant="contained" color="primary">
+        MUI Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
